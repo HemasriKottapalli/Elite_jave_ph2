199747310f69f5a4f460393141ee1849c9d029d0
HemasriKottapalli <120649018+HemasriKottapalli@users.noreply.github.com>
2023-06-15 20:54:32 -0700

    Add files via upload
    Day7 phase2

---
+++ day7/introToJDBc.java
@@ -0,0 +1,21 @@
+package com.day7;
+
+public class introToJDBc {
+
+	public static void main(String[] args) {
+		/*
+		                 JDBC API
+		  Java Program -------------->DataBase
+		  
+		  JDBC API has 
+		    -->Driver Manager--->MySql driver
+		                     \------->Oracle Driver
+		                     \-------->DB2 Driver
+		                     
+		    # if we use MySql we've to use mySql driver
+		    #these are external jar files that are not present in jre system lib
+		 */
+
+	}
+
+}
